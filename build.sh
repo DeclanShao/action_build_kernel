fcf6026e3f2f4b72c8dd0c14401eebcb7784e15e
DeclanShao <2378677167@qq.com>
2023-01-12 11:44:37 +0800

    Init project

---
+++ build.sh
@@ -0,0 +1,13 @@
+echo "===================Setup Export========================="
+export ARCH=arm64
+export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-none-linux-gnu-
+export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-none-eabi-
+
+echo "=========================Clean========================="
+rm -rf $KERNEL_PATH/out/ *.zip
+make mrproper && git reset --hard HEAD
+
+echo "=========================make========================="
+make O=out mido_defconfig
+make O=out
+
