d8f6569a11da9bf22ee0806d9cf81eac6ef8a010
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2023-01-13 16:02:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 echo "===================Setup Export========================="
 export ARCH=arm64
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export CC=$GITHUB_WORKSPACE/kernel/clang/bin/clang 
 export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-none-linux-gnu-
 export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-none-eabi-
 
