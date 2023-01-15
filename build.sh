c975e6d6800ae66e294d26f3be47306dd0a728bf
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2023-01-15 22:16:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 echo "===================Setup Export========================="
+export CLANG_PATH=~/prelude-clang
+export PATH=${CLANG_PATH}/bin:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export SUBARCH=arm64
 #export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-none-linux-gnu-
@@ -11,5 +14,5 @@ make mrproper && git reset --hard HEAD
 echo "=========================make========================="
 #make O=out tb8704_defconfig
 #make O=out
-make O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/bin/arm-linux-gnueabi- LD=ld.lld tb8704_defconfig
+make O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/prelude-clang/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/prelude-clang/bin/arm-linux-gnueabi- LD=ld.lld tb8704_defconfig
 make O=out
