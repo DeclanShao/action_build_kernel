6500280078e0a175dc63df09311c836423f7f185
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2023-01-15 23:09:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 echo "===================Setup Export========================="
 export ARCH=arm64
 export SUBARCH=arm64
-export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-linux-androideabi-
-#export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-none-linux-gnu-
-#export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-none-eabi-
+export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-none-linux-gnu-
+export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-none-eabi-
 
 echo "=========================Clean========================="
 rm -rf $KERNEL_PATH/out/ *.zip
