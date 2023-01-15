c7cbacd3a218cca56c477f5b0931864741226b25
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2023-01-15 22:23:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ export PATH=${CLANG_PATH}/bin:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export SUBARCH=arm64
+export CC="ccache clang"
+export CXX="ccache clang++"
+export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/prelude-clang/bin/aarch64-linux-gnu- 
+export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/prelude-clang/bin/arm-linux-gnueabi-
 #export CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/tool/aarch/bin/aarch64-none-linux-gnu-
 #export CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/tool/arm/bin/arm-none-eabi-
 
@@ -12,7 +16,6 @@ rm -rf $KERNEL_PATH/out/ *.zip
 make mrproper && git reset --hard HEAD
 
 echo "=========================make========================="
-#make O=out tb8704_defconfig
-#make O=out
-make O=out CC="ccache clang" CXX="ccache clang++" CROSS_COMPILE=$GITHUB_WORKSPACE/kernel/prelude-clang/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=$GITHUB_WORKSPACE/kernel/prelude-clang/bin/arm-linux-gnueabi- LD=ld.lld tb8704_defconfig
+make O=out tb8704_defconfig
 make O=out
+
